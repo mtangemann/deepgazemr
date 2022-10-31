f8d22b085929b0288ad025de1a442d0e4c63074a
Matthias Tangemann <git@mtangemann.de>
2022-10-31 08:28:46 +0100

    load checkpoint using absolute path
    Loading the checkpoint using a relative path within the repository sometimes fails.

--- hubconf.py
+++ hubconf.py
@@ -2,9 +2,15 @@
 
 # TODO how to list dependencies here?
 
+from pathlib import Path
+
 import torch
 
 from deepgazemr import DeepGazeMR as _DeepGazeMR
+
+REPOSITORY_PATH = Path(__file__).parent
+CHECKPOINT_PATH = REPOSITORY_PATH / 'data' / 'deepgazemr-ledov.pt'
+CENTER_BIAS_PATH = REPOSITORY_PATH / 'data' / 'center-bias-ledov.pt'
 
 
 def DeepGazeMR(pretrained=True):
@@ -12,11 +18,11 @@ def DeepGazeMR(pretrained=True):
 
     if pretrained:
         # load checkpoint with model weights
-        checkpoint = torch.load('data/deepgazemr-ledov.pt')
+        checkpoint = torch.load(CHECKPOINT_PATH)
         model.load_state_dict(checkpoint['model_state_dict'])
 
         # load LEDOV center bias
-        center_bias = torch.load('data/center-bias-ledov.pt')
+        center_bias = torch.load(CENTER_BIAS_PATH)
         model.center_bias = center_bias
 
     return model
